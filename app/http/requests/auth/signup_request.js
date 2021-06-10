c5163a58f4bb8ae7ca1c66780386e72e0519fe96
Mohammad Ali <mdali2016.227@gmail.com>
2021-06-10 16:01:55 +0600

    [Add] validation rule array

--- app/http/requests/auth/signup_request.js
+++ app/http/requests/auth/signup_request.js
@@ -8,10 +8,11 @@ class SignupRequest extends Request{
             lastName: 'required|string',
             email: 'required|email|unique:user',
             phoneCode: 'required|string|regex:/^[+]{1}[0-9]{3}$/',
-            phone: 'required|string|regex:/^(1){1}[1-9]{1}[0-9]{8}$/',
+            phone: 'required|string|unique:user|regex:/^(1){1}[1-9]{1}[0-9]{8}$/',
             // role: 'required|in:'+[ADMIN_ROLE,USER_ROLE].join(','),
             password: 'required|string|min:8',
             confirmPassword: 'required|same:password',
+            arr: 'required|array',
         })
     }
 }
